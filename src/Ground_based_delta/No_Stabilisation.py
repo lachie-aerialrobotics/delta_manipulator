62e556465a75e2630a3b09972fe014990bde65ff
Lachie <lachlan.orr15@imperial.ac.uk>
2022-01-05 17:26:35 +0000

    moving code to drone

--- src/Ground_based_delta/No_Stabilisation.py
+++ src/Ground_based_delta/No_Stabilisation.py
@@ -3,17 +3,13 @@ import rospy
 import numpy as np
 
 import tf2_ros
-from tf2_geometry_msgs import do_transform_point
-
+from quaternion_functions import qv_mult, q_mult, q_conjugate
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PointStamped, PoseStamped, TransformStamped
 
 #script calculates required end-effector position of manipulator to stabilise base perturbations using tf2 frames
 class Controller:
     def __init__(self):
-        self.tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(self.tfBuffer)
-
         #broadcast static frames
         br_static = tf2_ros.StaticTransformBroadcaster()
         tf_base2manip = transform_msg(
@@ -21,11 +17,11 @@ class Controller:
             0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 1.0
         )
-        q = quaternion_from_euler(np.pi, 0.0, 0.0)
+        self.q = quaternion_from_euler(np.pi, 0.0, 0.0)
         tf_map2base = transform_msg(
             "map", "base_link",
             0.0, 0.0, 0.0,
-            q[0], q[1], q[2], q[3]
+            self.q[0], self.q[1], self.q[2], self.q[3]
         )
   
         br_static.sendTransform([tf_map2base, tf_base2manip])
@@ -37,15 +33,16 @@ class Controller:
         self.tip_sp_sub = rospy.Subscriber('/tooltip/setpoint_position/global', PointStamped, self.callback, tcp_nodelay=True) 
 
     def callback(self, tip_sp_msg):        
-        trans = self.tfBuffer.lookup_transform('map', 'manipulator', rospy.Time(), rospy.Duration(0.5))
-        do_transform_point(tip_sp_msg, trans)
+        p = np.asarray([tip_sp_msg.point.x, tip_sp_msg.point.y, tip_sp_msg.point.z])
+        p = qv_mult(self.q, p)
+
         #publish PointStamped message representing target of delta-manipulator relative to base
         pos_msg = PointStamped()
         pos_msg.header.frame_id = "manipulator"
         pos_msg.header.stamp = rospy.Time.now()
-        pos_msg.point.x = tip_sp_msg.point.x
-        pos_msg.point.y = tip_sp_msg.point.y
-        pos_msg.point.z = tip_sp_msg.point.z
+        pos_msg.point.x = p[0]
+        pos_msg.point.y = p[1]
+        pos_msg.point.z = p[2]
         self.pub_tip_pos.publish(pos_msg) 
 
 def transform_msg(header, child, tx, ty, tz, rx, ry, rz, rw): #function populates transformStamped message
