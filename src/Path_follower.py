d5b4f4f070fab830a7cfcfcae0341255a04e30b4
Lachie <lachlan.orr15@imperial.ac.uk>
2022-01-23 11:35:49 +0000

    changed trajectory config for sperbank demo

--- src/Path_follower.py
+++ src/Path_follower.py
@@ -185,7 +185,10 @@ class Setpoint:
 
             drone2traj = p_traj_start - self.p
 
-            drone2traj_norm = drone2traj / np.linalg.norm(drone2traj)
+            if drone2traj != 0.0:
+                drone2traj_norm = drone2traj / np.linalg.norm(drone2traj)
+            else:
+                drone2traj_norm = np.asarray([0.0, 0.0, 1.0])
 
             q_diff = q_mult(self.q, q_conjugate(q_traj_start))
 
