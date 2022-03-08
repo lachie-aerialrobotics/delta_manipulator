a99ef432142f9163a7b8fe321b6def381b2098ff
Lachie <lachlan.orr15@imperial.ac.uk>
2022-03-08 17:01:07 +0000

    final commit before lidar testing

--- src/Lidar_msg_converter.py
+++ src/Lidar_msg_converter.py
@@ -23,7 +23,6 @@ class Converter:
         lidar_offset_y = rospy.get_param('/manipulator/lidar_offset_y')
         lidar_offset_z = rospy.get_param('/manipulator/lidar_offset_z')
         tf_odom2base_link = self.transform_msg("os_sensor", "base_link", lidar_offset_x, lidar_offset_y, lidar_offset_z, 0, 0, 0, 1)
-
         tf_map2odom = self.transform_msg("map", "odom", 0, 0, 0, 0, 0, 0, 1)
   
         br_static.sendTransform([tf_odom2base_link, tf_map2odom])
