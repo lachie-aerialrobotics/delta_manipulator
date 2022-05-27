6aa2c39a4532b57018dc4931c282cc85f978b899
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2022-05-27 17:09:03 +0200

    added path generator for delta

--- src/Tooltip_path_generator.py
+++ src/Tooltip_path_generator.py
@@ -2,7 +2,7 @@
 import rospy
 import numpy as np
 from nav_msgs.msg import Path
-
+from geometry_msgs.msg import PoseStamped
 class Setpoint:
     def __init__(self):   
         #retrieve params from parameter server
@@ -18,20 +18,26 @@ class Setpoint:
         self.drone_path_sub = rospy.Subscriber('/path/drone', Path, self.path_callback, tcp_nodelay=True)
 
     def path_callback(self, path_msg):
-        self.index = 0
-        while self.index < len(self.path_msg.pose):
-            if self.index == 0:
+        index = 0
+        manip_path_msg = Path()
+        manip_path_msg.header.stamp = path_msg.header.stamp
+        manip_path_msg.header.frame_id = path_msg.header.frame_id
+        while index < len(path_msg.poses):
+            if index == 0:
                 rospy.loginfo("Starting trajectory...")
-            self.manip_path_msg = Path()
-            self.manip_path_msg.poses[self.index].pose.position.x = self.path_msg.poses[self.index].pose.position.x + self.tip_init_x 
-            self.manip_path_msg.poses[self.index].pose.position.y = self.path_msg.poses[self.index].pose.position.y + self.tip_init_y
-            self.manip_path_msg.poses[self.index].pose.position.z = self.path_msg.poses[self.index].pose.position.z + self.tip_init_z
-            self.index += 1
+            new_position = PoseStamped()
+            new_position.header.frame_id = path_msg.header.frame_id
+            new_position.header.stamp = path_msg.header.stamp
+            new_position.pose.position.x = path_msg.poses[index].pose.position.x + self.tip_init_x 
+            new_position.pose.position.y = path_msg.poses[index].pose.position.y + self.tip_init_y
+            new_position.pose.position.z = path_msg.poses[index].pose.position.z + self.tip_init_z  
+            manip_path_msg.poses.append(new_position)
+            index += 1
         rospy.loginfo("Completed computing delta trajectory!")
-        self.manip_path_pub.publish(self.manip_path_msg)
+        self.manip_path_pub.publish(manip_path_msg)
 
 if __name__ == '__main__': #initialise node
-    rospy.init_node('joystick_node', anonymous=True)
+    rospy.init_node('tooltip_path_node', anonymous=True)
     s = Setpoint()
     rospy.spin()
 
